dcb6c28ee32ec580562df9d78c89d4e517ae372d
fcarli3 <francescocarli79@gmail.com>
2021-12-04 15:50:27 +0100

    added celery worker and beat to docker-compose

--- Dockerfile
+++ Dockerfile
@@ -7,15 +7,19 @@ LABEL version="1.0"
 LABEL description="Message in a Bottle Lottery Microservice"
 
 # creating the environment
-COPY . /app
-# setting the workdir
-WORKDIR /app
-RUN chmod +x celery.sh
+RUN mkdir -p /usr/src/app
+WORKDIR /usr/src/app
+
+COPY requirements.txt /usr/src/app/
+COPY requirements.prod.txt /usr/src/app/
+
 # installing all requirements
 RUN ["pip", "install", "-r", "requirements.prod.txt"]
 
+COPY . /usr/src/app
+
 # exposing the port
 EXPOSE 5002/tcp
-# Main command
-CMD  ./celery.sh
 
+# Main command
+CMD ["gunicorn", "--config", "gunicorn.conf.py", "wsgi:app"]
--- swagger_server/background.py
+++ swagger_server/background.py
@@ -1,19 +1,25 @@
 from celery import Celery
 from datetime import timedelta
-BACKEND = BROKER = 'redis://redis:6379'
-celery = Celery(__name__, backend=BACKEND, broker=BROKER)
+import celery
+from swagger_server import create_app
+from os import environ
+
+environ.setdefault('CELERY_CONFIG_MODULE', 'swagger_server.background_config')
+
+celery = Celery(__name__)
+celery.config_from_envvar('CELERY_CONFIG_MODULE')
+_APP = create_app()
 
 
 @celery.on_after_configure.connect
 def setup_periodic_task(sender, **kwargs):
-    sender.add_periodic_task(timedelta(seconds=60),
+    sender.add_periodic_task(timedelta(seconds=10),
                              increase_trials, expires=10)
 
 
 @celery.task
 def increase_trials():
-    from swagger_server import create_app
-    app = create_app()
-    from swagger_server.dao.lottery_manager import LotteryManager
+    with _APP.app_context():
+        from swagger_server.dao.lottery_manager import LotteryManager
 
-    LotteryManager.update_trials()
+        LotteryManager.update_trials()
